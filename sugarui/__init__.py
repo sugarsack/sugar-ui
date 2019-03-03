4a43833d3e544df6eb77e07f4910b1a441131f23
Bo Maryniuk <bo@suse.de>
2019-03-03 21:45:16 +0100

    Extend app to be able to switch forms and build menus

--- sugarui/__init__.py
+++ sugarui/__init__.py
@@ -7,11 +7,61 @@ and run as a sub-app from the nested command-line.
 
 import npyscreen
 from sugarui.dashboard import Dashboard
+from sugarui.windows.sysoverview import SystemOverviewForm
 
 
 class SugarUI(npyscreen.StandardApp):
     """
     Sugar UI class.
     """
+    def __init__(self):
+        npyscreen.StandardApp.__init__(self)
+        self._app_name = "Sugar"
+        self._forms = [
+            # (id, class, args, keywords, title, shortcut, classname)
+        ]
+
+    def register_form(self, fid, cls, *args, **keywords):
+        """
+        Register form class to the menu data.
+
+        :param fid:
+        :param cls:
+        :param args:
+        :param keywords:
+        :return:
+        """
+        keywords["name"] = self.ftitle(keywords["name"])
+        self._forms.append((fid, cls, args, keywords, keywords.pop("title"), keywords.pop("shortcut"),
+                            "on_load_{}".format(cls.__name__.lower())))
+
+    def ftitle(self, text):
+        """
+        Format screen form title.
+
+        :param text:
+        :return:
+        """
+        return "{} - {}".format(self._app_name, text)
+
+    def init_forms(self):
+        """
+        Init forms.
+
+        :return:
+        """
+        for form_data in self._forms:
+            fid, cls, args, kwargs = form_data[:4]
+            self.addForm(fid, cls, *args, **kwargs)
+
     def onStart(self):
-        self.addForm(Dashboard.id, Dashboard, name="Sugar Dashboard, v0.0.0")
+        """
+        On start of the application.
+
+        :return:
+        """
+        self.register_form(Dashboard.id, Dashboard,
+                           title="Dashboard", shortcut="^D", name="Dashboard, v0.0.0")
+        self.register_form(SystemOverviewForm.id, SystemOverviewForm,
+                           title="System Overview", shortcut="^O", name="System Overview")
+        self.init_forms()
