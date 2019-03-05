578b24452c39a5f28b8fd8af6068a87cbedbb7c3
Bo Maryniuk <bo@suse.de>
2019-03-05 17:47:35 +0100

    Add better viewed visual text field that can colorise its background

--- sugarui/widgets/textfields.py
+++ sugarui/widgets/textfields.py
@@ -9,7 +9,7 @@ import npyscreen
 
 class ColoredTextField(npyscreen.Textfield):
     """
-    Colorised text field.
+    Colorised text field (highlighted data)
     """
     syntax_highlighting = True
 
@@ -44,3 +44,94 @@ class ColoredTextField(npyscreen.Textfield):
         """
         self.colorise()
         super(npyscreen.Textfield, self).update(clear, cursor)
+
+
+class VisualTextField(npyscreen.Textfield):
+    """
+    Text field with colored background.
+    """
+    def __init__(self, *args, **kwargs):
+        kwargs["color"] = "CAUTIONHL"
+        kwargs["max_height"] = 1
+        npyscreen.Textfield.__init__(self, *args, **kwargs)
+
+    def _print(self):
+        """
+        Create text content within the field.
+
+        :return:
+        """
+        self._field_space()
+        width_of_char_to_print = 0
+
+        string_to_print = self._get_string_to_print()
+        if not string_to_print:
+            return None
+
+        string_to_print = self.display_value(self.value)[
+                          self.begin_at:self.maximum_string_length + self.begin_at - self.left_margin]
+
+        column = 0
+        place_in_string = 0
+        if self.syntax_highlighting:
+            self.update_highlighting(start=self.begin_at,
+                                     end=self.maximum_string_length + self.begin_at - self.left_margin)
+            while column <= (self.maximum_string_length - self.left_margin):
+                if not string_to_print or place_in_string > len(string_to_print) - 1:
+                    break
+                width_of_char_to_print = self.find_width_of_char(string_to_print[place_in_string])
+                if column - 1 + width_of_char_to_print > self.maximum_string_length:
+                    break
+                try:
+                    highlight = self._highlightingdata[self.begin_at + place_in_string]
+                except IndexError:
+                    highlight = curses.A_NORMAL
+                self.parent.curses_pad.addstr(self.rely, self.relx + column + self.left_margin,
+                                              self._print_unicode_char(string_to_print[place_in_string]),
+                                              highlight)
+                column += self.find_width_of_char(string_to_print[place_in_string])
+                place_in_string += 1
+        else:
+            if self.do_colors():
+                if self.show_bold and self.color == 'DEFAULT':
+                    color = self.parent.theme_manager.findPair(self, 'BOLD') | curses.A_BOLD
+                elif self.show_bold:
+                    color = self.parent.theme_manager.findPair(self, self.color) | curses.A_BOLD
+                elif self.important:
+                    color = self.parent.theme_manager.findPair(self, 'IMPORTANT') | curses.A_BOLD
+                else:
+                    color = self.parent.theme_manager.findPair(self)
+            else:
+                if self.important or self.show_bold:
+                    color = curses.A_BOLD
+                else:
+                    color = curses.A_NORMAL
+            color = color | curses.A_STANDOUT | curses.A_BOLD | curses.A_DIM
+            while column <= (self.maximum_string_length - self.left_margin):
+                if not string_to_print or place_in_string > len(string_to_print) - 1:
+                    if self.highlight_whole_widget:
+                        self.parent.curses_pad.addstr(self.rely, self.relx + column + self.left_margin, ' ', color)
+                        column += width_of_char_to_print
+                        place_in_string += 1
+                        continue
+                    else:
+                        break
+
+                width_of_char_to_print = self.find_width_of_char(string_to_print[place_in_string])
+                if column - 1 + width_of_char_to_print > self.maximum_string_length:
+                    break
+                self.parent.curses_pad.addstr(self.rely, self.relx + column + self.left_margin,
+                                              self._print_unicode_char(string_to_print[place_in_string]), color)
+                column += width_of_char_to_print
+                place_in_string += 1
+
+    def _field_space(self):
+        """
+        Create a field space.
+
+        :return:
+        """
+        line = " " * self.width
+        self.add_line(self.rely, self.relx, line, self.make_attributes_list(
+            line, self.parent.theme_manager.findPair(
+                self, self.color) | curses.A_STANDOUT | curses.A_BOLD | curses.A_DIM), self.width)
