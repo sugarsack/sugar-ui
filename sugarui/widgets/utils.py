6b03d99565957e22d7563cab9593fed08f0b3aff
Bo Maryniuk <bo@suse.de>
2019-03-08 20:11:07 +0100

    Update button standard render

--- sugarui/widgets/utils.py
+++ sugarui/widgets/utils.py
@@ -8,11 +8,18 @@ TYPE_BTN_REJECT = "\u25c9"
 TYPE_BTN_SELECT = "\u25c8"
 
 
-def make_button_label(label: str, type=TYPE_BTN_CONFIRM) -> str:
+def make_button_label(label: str, type=TYPE_BTN_CONFIRM, selected: bool = True) -> str:
     """
     Turns any label into a button label standard.
 
     :param label: Text
     :return: button label string
     """
-    return "{label} {f}".format(f=type, label=(label or "* ERROR *").strip())
+    if selected:
+        ls = "\u25B8"
+        rs = "\u25C2"
+    else:
+        ls = "\u25B9"
+        rs = "\u25C3"
+
+    return "{ls} {label} {f}  {rs}".format(f=type, label=(label or "* ERROR *").strip(), ls=ls, rs=rs)
